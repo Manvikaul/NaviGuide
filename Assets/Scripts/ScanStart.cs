6793a7caeff5a842445b8193a35abfbf8bf888a2
Manvikaul <47503962+Manvikaul@users.noreply.github.com>
2020-06-11 01:27:22 +0530

    version 1

--- Assets/Scripts/ScanStart.cs
+++ Assets/Scripts/ScanStart.cs
@@ -1,33 +1,39 @@
-﻿using System.Collections;
+﻿using GoogleARCore;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class StartScript : MonoBehaviour
+//class used at start of app to initialize position
+public class ScanStart : MonoBehaviour
 {
     public GameObject ardevice; //ARCore device gameobject
 
     private bool camAvailable; //bool used for seeing if rendering with camera is possible
     private WebCamTexture backCam; //used to obtain video from device camera
-    private Texture defaultBackground;
+    private Texture defaultBackground; 
 
     public RawImage background; // where to render to
     public AspectRatioFitter fit; //fit rendered view to screen
-    public ImageRecognition imgRec; //object used to access method for setting location 
 
-    // Start is called before the first frame update
-    void Start()
+    public ImageRecognition imgRec; //object used to access method for setting location
+    public GameObject text; // start text
+    public GameObject scanOverlay; //start overlay
+
+    //setup logic to capture camera video
+    private void Start()
     {
         defaultBackground = background.texture;
         WebCamDevice[] devices = WebCamTexture.devices;
-        if (devices.Length == 0)
+
+        if(devices.Length == 0)
         {
             Debug.Log("No camera detected");
             camAvailable = false;
             return;
         }
 
-        for (int i = 0; i < devices.Length; i++)
+        for(int i = 0; i < devices.Length; i++)
         {
             if (!devices[i].isFrontFacing)
             {
@@ -35,7 +41,7 @@ public class StartScript : MonoBehaviour
             }
         }
 
-        if (backCam == null)
+        if(backCam == null)
         {
             Debug.Log("unable to find backcam");
             return;
@@ -43,21 +49,24 @@ public class StartScript : MonoBehaviour
 
         backCam.Play();
         background.texture = backCam;
+
         camAvailable = true;
     }
 
-
-    // Update is called once per frame
-    void Update()
+    //if camera setup render each frame the obtained images
+    private void Update()
     {
         if (!camAvailable)
         {
             return;
         }
+
         float ratio = (float)backCam.width / (float)backCam.height;
         fit.aspectRatio = ratio;
-        float scaleY = backCam.videoVerticallyMirrored ? -1f : 1f;
+
+        float scaleY = backCam.videoVerticallyMirrored ? -1f: 1f;
         background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
         int orient = -backCam.videoRotationAngle;
         background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
 
@@ -67,7 +76,21 @@ public class StartScript : MonoBehaviour
         {
             ardevice.GetComponent<ARCoreSession>().enabled = true;
             background.gameObject.SetActive(false);
+            text.SetActive(false);
+            scanOverlay.SetActive(false);
             this.gameObject.SetActive(false);
-        }
+        } 
     }
+
+    //used for testing
+    public void OnClick()
+    {
+        ardevice.GetComponent<ARCoreSession>().enabled = true;
+        background.gameObject.SetActive(false);
+        text.SetActive(false);
+        scanOverlay.SetActive(false);
+        this.gameObject.SetActive(false);
+    }
+
+    
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# NaviGuide
+# IndoorNavigation
+This repo contains the project I made during my month of internship at Craftworkz. This is a prototype and not clean code.
